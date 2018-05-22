e8f3bf5ed6def2aef1b4fb3a6dc63a875c8c1d97
arashilen <arashilen@hotmail.com>
2018-05-22 22:17:32 +0800

    submission draft includes source category dir

--- GLRC/remapping_submission_csv.py
+++ GLRC/remapping_submission_csv.py
@@ -22,12 +22,17 @@ with open('test.csv', newline='') as csv_file:
     for row in csv_reader:
         assert len(row) == 2
         id = row[0]
-        landmarks = id_to_landmarks[id] if id in id_to_landmarks else ""
-        if landmarks:
-            score = float(landmarks.split(" ")[1])
+        landmark = id_to_landmarks[id] if id in id_to_landmarks else ""
+        if landmark:
+            tokens = landmark.split(" ")
+            infer_category = tokens[1]
+            score_str = tokens[2]
+            score = float(score_str)
             if score < score_min:
-                landmarks = ""
-        write_list.append([id, landmarks])
+                landmark = ""
+            else:
+                landmark = " ".join([infer_category, score_str])
+        write_list.append([id, landmark])
 
 with open('submission_mapped_filter_by_score.csv', 'w', newline='') as f:
     write_list.insert(0, ["id", "landmarks"])
