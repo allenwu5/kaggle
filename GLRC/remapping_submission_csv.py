5000e346b61a6474e466a483e099aa967bc72cc9
arashilen <arashilen@hotmail.com>
2018-05-28 21:27:11 +0800

    Remapping submission with Top 5

--- GLRC/remapping_submission_csv.py
+++ GLRC/remapping_submission_csv.py
@@ -1,8 +1,9 @@
 import csv
 
-id_to_landmarks = {}
+id_to_pred = {}
 write_list = []
-score_min = 0.0
+
+TOP_K = 5
 
 with open('submission.csv', newline='') as csv_file:
     csv_reader = csv.reader(csv_file)
@@ -11,7 +12,7 @@ with open('submission.csv', newline='') as csv_file:
 
     for row in csv_reader:
         assert len(row) == 2
-        id_to_landmarks[row[0]] = row[1].strip()
+        id_to_pred[row[0]] = row[1].strip()
 
 
 with open('test.csv', newline='') as csv_file:
@@ -21,20 +22,19 @@ with open('test.csv', newline='') as csv_file:
 
     for row in csv_reader:
         assert len(row) == 2
-        id = row[0]
-        landmark = id_to_landmarks[id] if id in id_to_landmarks else ""
-        if landmark:
-            tokens = landmark.split(" ")
-            infer_category = tokens[1]
-            score_str = tokens[2]
-            score = float(score_str)
-            if score < score_min:
-                landmark = ""
-            else:
-                landmark = " ".join([infer_category, score_str])
-        write_list.append([id, landmark])
+        img_id = row[0]
+        pred = id_to_pred[img_id] if img_id in id_to_pred else ""
+        if pred:
+            tokens = pred.split(" ")
+            top1_class = tokens[1]
+            top1_score = tokens[2]
+            topK_index = 3
+            topK_scores = [float(s) for s in tokens[topK_index:topK_index+TOP_K]]
 
-with open('submission_mapped_filter_by_score.csv', 'w', newline='') as f:
+            pred = " ".join([top1_class, top1_score])
+
+
+with open('submission_mapped.csv', 'w', newline='') as f:
     write_list.insert(0, ["id", "landmarks"])
 
     writer = csv.writer(f)
